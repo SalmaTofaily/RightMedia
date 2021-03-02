10d38601abc107e0fac5e2b81fe91e99d2647dfe
OMEN\Salma <salma.tofaily@gotocme.com>
2021-03-02 02:35:12 +0200

    With love.. Kicking off Right Media Solution!

---
+++ RightMedia/Encoder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Encoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! By Right Media we will reimagine the watching experience.\nAuthor: Salma Tofaily");
+        }
+    }
+}
